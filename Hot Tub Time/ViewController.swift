5e8f1e2e2807a54b5f6b94aedd23bdb4a119721f
Joseph Miller <josephclaymiller@gmail.com>
2021-12-20 08:34:09 -0800

    Initial Commit

---
+++ Hot Tub Time/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hot Tub Time
+//
+//  Created by Joseph Miller on 12/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
